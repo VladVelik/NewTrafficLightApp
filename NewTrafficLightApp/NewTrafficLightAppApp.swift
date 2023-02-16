bde5d4381f1a5d781061bc7ed3c8cf201d326ab9
VladVelik <sosinvladik@yandex.ru>
2023-02-16 15:35:50 +0300

    Initial Commit

---
+++ NewTrafficLightApp/NewTrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTrafficLightAppApp.swift
+//  NewTrafficLightApp
+//
+//  Created by Sosin Vladislav on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewTrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
